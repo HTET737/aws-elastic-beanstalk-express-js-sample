3cd68de916ff46fc883e125900c98441c1fc4811
HTET737 <htetkyaw2099@gmail.com>
2026-06-13 23:34:58 +0900

    A change of Heart

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('BYE'));
+app.get('/', (req, res) => res.send('took a picture of your salad and put it on the internet'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
