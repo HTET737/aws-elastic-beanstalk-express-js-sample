9d833dbfc3af228805ea565cd4602c74814ed50d
HTET737 <htetkyaw2099@gmail.com>
2026-06-13 23:30:31 +0900

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('HELLO WORLD!'));
+app.get('/', (req, res) => res.send('BYE'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
